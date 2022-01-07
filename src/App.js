9008d7e0c8c87a3fc9cba60feb1b2a5d4be67c4d
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-01-08 00:38:28 +0530

    Made initial changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React from 'react'
+// import './styles/app.scss'
+import Player from './components/Player';
+import Song from './components/Song';
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello</h1>
+    <Song/>
+    <Player/>
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Song = () => {
+    return (
+        <>
+            <div className="song-container">
+                <h1>Pictures</h1>
+                <h1>Song name</h1>
+                <h1>Artist</h1>
+            </div>
+        </>
+    )
+}
+
+export default Song
